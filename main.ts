e0dd737fff2d3bcdc0dac74f784f88f6fac0f769
thatstrangebrit <peterretter4@outlook.com>
2025-10-28 08:41:21 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,34 +1,36 @@
 function Forward () {
-    motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, Speed)
-    motor.MotorRun(motor.Motors.M2, motor.Dir.CCW, Speed)
-    motor.MotorRun(motor.Motors.M3, motor.Dir.CCW, Speed)
-    motor.MotorRun(motor.Motors.M4, motor.Dir.CCW, Speed)
+    motor.MotorRun(motor.Motors.M1, motor.Dir.CW, Speed)
+    motor.MotorRun(motor.Motors.M2, motor.Dir.CW, Speed)
+    motor.MotorRun(motor.Motors.M3, motor.Dir.CW, Speed)
+    motor.MotorRun(motor.Motors.M4, motor.Dir.CW, Speed)
 }
 function TurnLeft () {
     motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, Speed)
     motor.MotorRun(motor.Motors.M2, motor.Dir.CW, Speed)
     motor.MotorRun(motor.Motors.M3, motor.Dir.CCW, Speed)
     motor.MotorRun(motor.Motors.M4, motor.Dir.CW, Speed)
-    basic.pause(400)
+    basic.pause(250)
+    Stop()
 }
 function Backwards () {
-    motor.MotorRun(motor.Motors.M1, motor.Dir.CW, Speed)
-    motor.MotorRun(motor.Motors.M2, motor.Dir.CW, Speed)
-    motor.MotorRun(motor.Motors.M3, motor.Dir.CW, Speed)
-    motor.MotorRun(motor.Motors.M4, motor.Dir.CW, Speed)
+    motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, Speed)
+    motor.MotorRun(motor.Motors.M2, motor.Dir.CCW, Speed)
+    motor.MotorRun(motor.Motors.M3, motor.Dir.CCW, Speed)
+    motor.MotorRun(motor.Motors.M4, motor.Dir.CCW, Speed)
 }
 function TurnRight () {
     motor.MotorRun(motor.Motors.M1, motor.Dir.CW, Speed)
     motor.MotorRun(motor.Motors.M2, motor.Dir.CCW, Speed)
     motor.MotorRun(motor.Motors.M3, motor.Dir.CW, Speed)
     motor.MotorRun(motor.Motors.M4, motor.Dir.CCW, Speed)
-    basic.pause(400)
+    basic.pause(250)
+    Stop()
 }
 function Stop () {
-    motor.motorStopAll()
+	
 }
-let SonarDistance_Left = 0
 let SonarDistance_Right = 0
+let SonarDistance_Left = 0
 let SonarDistance_Forward = 0
 let Speed = 0
 Speed = 75
@@ -39,33 +41,31 @@ basic.forever(function () {
     DigitalPin.P1,
     PingUnit.Centimeters
     )
-    SonarDistance_Right = sonar.ping(
+    basic.pause(30)
+    SonarDistance_Left = sonar.ping(
     DigitalPin.P2,
     DigitalPin.P8,
     PingUnit.Centimeters
     )
-    SonarDistance_Left = sonar.ping(
+    basic.pause(30)
+    SonarDistance_Right = sonar.ping(
     DigitalPin.P12,
     DigitalPin.P13,
     PingUnit.Centimeters
     )
-})
-basic.forever(function () {
-    if (SonarDistance_Left < MinDistance) {
-        Stop()
+    basic.pause(30)
+    // Left-hand rule maze solving
+    if (SonarDistance_Left > MinDistance) {
         TurnLeft()
         Forward()
-    } else if (SonarDistance_Forward < MinDistance) {
+    } else if (SonarDistance_Forward > MinDistance) {
         Forward()
-    } else if (SonarDistance_Right < MinDistance) {
-        Stop()
+    } else if (SonarDistance_Right > MinDistance) {
         TurnRight()
         Forward()
     } else {
-        Stop()
         Backwards()
-        basic.pause(500)
-        Stop()
+        basic.pause(300)
         TurnRight()
     }
 })
