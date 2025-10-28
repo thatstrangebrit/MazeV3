1169608e341f9a2893cab5eca8435ccdb04dfe23
thatstrangebrit <peterretter4@outlook.com>
2025-10-28 08:55:28 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,71 +1,64 @@
+function Left () {
+    motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, Speed)
+    motor.MotorRun(motor.Motors.M2, motor.Dir.CW, Speed)
+    motor.MotorRun(motor.Motors.M3, motor.Dir.CCW, Speed)
+    motor.MotorRun(motor.Motors.M4, motor.Dir.CW, Speed)
+}
 function Forward () {
     motor.MotorRun(motor.Motors.M1, motor.Dir.CW, Speed)
     motor.MotorRun(motor.Motors.M2, motor.Dir.CW, Speed)
     motor.MotorRun(motor.Motors.M3, motor.Dir.CW, Speed)
     motor.MotorRun(motor.Motors.M4, motor.Dir.CW, Speed)
 }
-function TurnLeft () {
-    motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, Speed)
-    motor.MotorRun(motor.Motors.M2, motor.Dir.CW, Speed)
-    motor.MotorRun(motor.Motors.M3, motor.Dir.CCW, Speed)
-    motor.MotorRun(motor.Motors.M4, motor.Dir.CW, Speed)
-    basic.pause(250)
-    Stop()
-}
-function Backwards () {
-    motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, Speed)
-    motor.MotorRun(motor.Motors.M2, motor.Dir.CCW, Speed)
-    motor.MotorRun(motor.Motors.M3, motor.Dir.CCW, Speed)
-    motor.MotorRun(motor.Motors.M4, motor.Dir.CCW, Speed)
-}
-function TurnRight () {
+function Right () {
     motor.MotorRun(motor.Motors.M1, motor.Dir.CW, Speed)
     motor.MotorRun(motor.Motors.M2, motor.Dir.CCW, Speed)
     motor.MotorRun(motor.Motors.M3, motor.Dir.CW, Speed)
     motor.MotorRun(motor.Motors.M4, motor.Dir.CCW, Speed)
-    basic.pause(250)
-    Stop()
 }
 function Stop () {
-	
+    motor.motorStopAll()
 }
-let SonarDistance_Right = 0
-let SonarDistance_Left = 0
-let SonarDistance_Forward = 0
+function Backward () {
+    motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, Speed)
+    motor.MotorRun(motor.Motors.M2, motor.Dir.CCW, Speed)
+    motor.MotorRun(motor.Motors.M3, motor.Dir.CCW, Speed)
+    motor.MotorRun(motor.Motors.M4, motor.Dir.CCW, Speed)
+}
+let distanceRight = 0
+let distanceLeft = 0
+let distanceFront = 0
 let Speed = 0
-Speed = 75
-let MinDistance = 13
+Speed = 100
 basic.forever(function () {
-    SonarDistance_Forward = sonar.ping(
+    distanceFront = sonar.ping(
     DigitalPin.P0,
     DigitalPin.P1,
     PingUnit.Centimeters
     )
-    basic.pause(30)
-    SonarDistance_Left = sonar.ping(
+    distanceLeft = sonar.ping(
     DigitalPin.P2,
     DigitalPin.P8,
     PingUnit.Centimeters
     )
-    basic.pause(30)
-    SonarDistance_Right = sonar.ping(
+    distanceRight = sonar.ping(
     DigitalPin.P12,
     DigitalPin.P13,
     PingUnit.Centimeters
     )
-    basic.pause(30)
-    // Left-hand rule maze solving
-    if (SonarDistance_Left > MinDistance) {
-        TurnLeft()
+    if (distanceFront <= 20 && distanceLeft <= 20 && distanceRight > 20) {
+        Right()
+        basic.pause(500)
+        Stop()
+    } else if (distanceFront <= 20 && distanceRight <= 20 && distanceLeft > 20) {
+        Left()
+        basic.pause(500)
+        Stop()
+    } else if (distanceLeft <= 20 && distanceRight <= 20 && distanceFront > 20) {
         Forward()
-    } else if (SonarDistance_Forward > MinDistance) {
-        Forward()
-    } else if (SonarDistance_Right > MinDistance) {
-        TurnRight()
-        Forward()
+        basic.pause(500)
+        Stop()
     } else {
-        Backwards()
-        basic.pause(300)
-        TurnRight()
+        Forward()
     }
 })
